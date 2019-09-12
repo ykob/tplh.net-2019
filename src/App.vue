5b6598096e7aa8f34858233c4d6542a2aaf5e45a
yoichi kobayashi <info@tplh.net>
2019-09-12 13:04:29 +0900

    made the basis of WebGL rendering on Vue component.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script>
+  import _ from 'lodash';
+
   export default {
     name: 'App',
     components: {
     },
     created: function() {
+      const { canvas, webgl } = this.$store.state;
+
+      document.body.append(canvas);
+      canvas.style = `
+        position: fixed;
+        top: 0;
+        left: 0;
+      `;
+
+      webgl.start(canvas);
+
+      this.resize();
+
+      webgl.play();
+
       // If finish the preload process, Start requestAnimationFrame Loop.
       this.update();
+
+      // On global events.
+      window.addEventListener('resize', _.debounce(this.resize, 100));
     },
     mounted: function() {
     },
@@ -13,38 +33,48 @@
     methods: {
       update: function() {
         // console.log(this.$router.currentRoute);
+        this.$store.state.webgl.update();
         requestAnimationFrame(this.update);
+      },
+      resize: function() {
+        const { canvas, resolution, webgl } = this.$store.state;
+
+        resolution.set(document.body.clientWidth, window.innerHeight);
+        canvas.width = resolution.x;
+        canvas.height = resolution.y;
+        webgl.resize(resolution);
       },
     },
   }
 </script>
 
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  .p-page
+    #nav
+      router-link(
+        to = '/'
+        )
+        |Home
+      router-link(
+        to = '/about/'
+        )
+        |About
+    router-view
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+  .p-page {
+    position: relative;
+    z-index: 10;
+  }
+  #nav {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
     }
   }
-}
 </style>
--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as THREE from 'three';
+
+import WebGL from '@/webgl/'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     globalId: 0,
+    canvas: document.createElement('canvas'),
+    resolution: new THREE.Vector2(),
+    webgl: new WebGL(),
   },
   mutations: {
     transit (state, opts) {
