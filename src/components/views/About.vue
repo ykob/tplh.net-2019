a87a92a229778c82e4342e8b9d1dd1e7d6c22d83
yoichi kobayashi <info@tplh.net>
2019-09-19 13:25:24 +0900

    clean up.

--- src/components/views/About.vue
+++ src/components/views/About.vue
@@ -1,23 +1,20 @@
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'About',
-  components: {
-  },
-  created: function() {
-    this.$store.commit('enableDarkColor', false);
-    this.$store.commit('showGlobalTitle', true);
-    this.$store.commit('transit', {
-      globalId: 1,
-    });
-  },
-  mounted: function() {
-  },
-  computed: {},
-  methods: {},
-}
+  export default {
+    name: 'About',
+    components: {
+    },
+    created: function() {
+      this.$store.commit('enableDarkColor', false);
+      this.$store.commit('showGlobalTitle', true);
+      this.$store.commit('transit', {
+        globalId: 1,
+      });
+    },
+    mounted: function() {
+    },
+    computed: {},
+    methods: {},
+  }
 </script>
 
 <template lang="pug">
