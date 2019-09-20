963f22acceb270febd681f7152fe5eee22dfbf1b
yoichi kobayashi <info@tplh.net>
2019-09-20 16:10:08 +0900

    made the works object.

---
+++ src/components/_const/WORKS.js
@@ -0,0 +1,27 @@
+import thumbSketchThreejs from '@assets/img/webgl/thumb_sketch_threejs.jpg';
+import thumbWarpdrive from '@assets/img/webgl/thumb_warpdrive.jpg';
+import thumbHassyadai from '@assets/img/webgl/thumb_hassyadai.jpg';
+
+export default [
+  {
+    key: 'sketchThreejs',
+    title: 'Sketch of Three.js',
+    description: '',
+    img: thumbSketchThreejs,
+    href: 'https://ykob.github.io/sketch-threejs/',
+  },
+  {
+    key: 'warpdrive',
+    title: 'WarpDrive',
+    description: '',
+    img: thumbWarpdrive,
+    href: 'https://warpdrive-project.jp/',
+  },
+  {
+    key: 'hassyadai',
+    title: 'HASSYADAI Inc.',
+    description: '',
+    img: thumbHassyadai,
+    href: 'https://hassyadai.com',
+  },
+]
