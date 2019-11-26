c4182e69910ed94da510d89d6b99b00840eff34c
yoichi kobayashi <info@tplh.net>
2019-11-26 14:38:20 +0900

    made the new component WorkOutline.

---
+++ src/components/atoms/WorkOutline.vue
@@ -0,0 +1,120 @@
+<script>
+  import zeroPadding from 'js-util/zeroPadding';
+
+  import Button from '@/components/atoms/Button.vue';
+  import SplitStr from '@/components/atoms/SplitStr.vue'
+
+  export default {
+    name: 'ContactTitle',
+    components: {
+      SplitStr,
+    },
+    props: {
+      isShown: {
+        type: Boolean,
+        default: false,
+      },
+      index: {
+        type: Number,
+        default: 0,
+      },
+      title: {
+        type: String,
+        default: '',
+      },
+      description: {
+        type: String,
+        default: '',
+      },
+      href: {
+        type: String,
+        default: '',
+      },
+    },
+    methods: {
+      getNumber(i) {
+        return zeroPadding(i + 1, 2);
+      }
+    },
+  };
+</script>
+
+<template lang="pug">
+  .p-work-outline
+    .p-work-outline__number
+      |Works &#35;{{ getNumber(index) }}
+    h1.p-work-outline__title
+      |{{ title }}
+    .p-work-outline__description
+      |{{ description }}
+    Button.p-work-outline__link(
+      tag = 'a'
+      :href = 'href'
+      target = '_blank'
+      )
+      |Show this.
+</template>
+
+<style lang="scss">
+  .p-work-outline {
+    position: absolute;
+    @include l-more-than-mobile {
+      top: 42%;
+      left: 50px;
+    }
+    @include l-mobile {
+    }
+    &__number {
+      line-height: 1;
+      @include fontSizeAll(21, 21, 14);
+      text-transform: uppercase;
+      letter-spacing: 0.16em;
+      @include l-more-than-mobile {
+        margin-bottom: 24px;
+      }
+      @include l-mobile {
+        margin-bottom: 12px;
+      }
+    }
+    &__title {
+      line-height: .9;
+      margin-top: 0;
+      margin-bottom: 0;
+      @include fontSizeAll(42, 42, 24);
+      letter-spacing: 0.14em;
+      @include l-more-than-mobile {
+        margin-bottom: 30px;
+      }
+      @include l-mobile {
+        margin-bottom: 20px;
+      }
+    }
+    &__title-typo {
+      opacity: 0;
+      transform: translate3d(0, .4em, 0) rotateY(70deg);
+      transition-property: opacity, transform;
+      .is-shown & {
+        opacity: 1;
+        transition-duration: 1s;
+        transition-timing-function: $easeOutQuad;
+        transform: translate3d(0, 0, 0) rotateY(0);
+      }
+      .view-leave-to & {
+        opacity: 0;
+        transition-duration: .6s;
+        transition-timing-function: $easeInQuad;
+        transform: translate3d(0, -.6em, 0) rotateY(-70deg);
+      }
+    }
+    &__description {
+      letter-spacing: 0.16em;
+      white-space: pre-wrap;
+      @include l-more-than-mobile {
+        margin-bottom: 20px;
+      }
+      @include l-mobile {
+        margin-bottom: 10px;
+      }
+    }
+  }
+</style>
--- src/components/views/Works.vue
+++ src/components/views/Works.vue
@@ -1,8 +1,7 @@
 <script>
   import _ from 'lodash';
-  import zeroPadding from 'js-util/zeroPadding';
-
-  import Button from '@/components/atoms/Button.vue';
+  
+  import WorkOutline from '@/components/atoms/WorkOutline.vue';
 
   export default {
     name: 'Works',
@@ -16,6 +15,7 @@
       ]
     },
     components: {
+      WorkOutline
     },
     created: function() {
       this.$store.commit('enableDarkColor', true);
@@ -45,67 +45,15 @@
     transition(
       name = 'show'
       )
-      .p-works-outline(
+      WorkOutline(
         v-for = 'item, index in $store.state.works'
         v-if = 'item.key === $route.params.key'
+        :index = 'index'
+        :title = 'item.title'
+        :description = 'item.description'
+        :href = 'item.href'
         )
-        .p-works-outline__number
-          |Works &#35;{{ getNumber(index) }}
-        h1.p-works-outline__title
-          |{{ item.title }}
-        .p-works-outline__description
-          |{{ item.description }}
-        Button.p-works-outline__link(
-          tag = 'a'
-          :href = 'item.href'
-          target = '_blank'
-          )
-          |Show this.
 </template>
 
 <style lang="scss">
-  .p-works-outline {
-    position: absolute;
-    @include l-more-than-mobile {
-      top: 42%;
-      left: 50px;
-    }
-    @include l-mobile {
-    }
-    &__number {
-      line-height: 1;
-      @include fontSizeAll(21, 21, 14);
-      text-transform: uppercase;
-      letter-spacing: 0.16em;
-      @include l-more-than-mobile {
-        margin-bottom: 24px;
-      }
-      @include l-mobile {
-        margin-bottom: 12px;
-      }
-    }
-    &__title {
-      line-height: .9;
-      margin-top: 0;
-      margin-bottom: 0;
-      @include fontSizeAll(42, 42, 24);
-      letter-spacing: 0.14em;
-      @include l-more-than-mobile {
-        margin-bottom: 30px;
-      }
-      @include l-mobile {
-        margin-bottom: 20px;
-      }
-    }
-    &__description {
-      letter-spacing: 0.16em;
-      white-space: pre-wrap;
-      @include l-more-than-mobile {
-        margin-bottom: 20px;
-      }
-      @include l-mobile {
-        margin-bottom: 10px;
-      }
-    }
-  }
 </style>
