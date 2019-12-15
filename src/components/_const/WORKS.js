cfa5ec5c63641a6a5191fb8a2f48a38bb2ee1a8a
yoichi kobayashi <info@tplh.net>
2019-12-15 22:04:59 +0900

    adjusted how to import img files in works json.

--- src/components/_const/WORKS.js
+++ src/components/_const/WORKS.js
@@ -1,27 +1,23 @@
-import thumbSketchThreejs from '@/assets/img/webgl/thumb_sketch_threejs.jpg';
-import thumbWarpdrive from '@/assets/img/webgl/thumb_warpdrive.jpg';
-import thumbHassyadai from '@/assets/img/webgl/thumb_hassyadai.jpg';
-
 export default [
   {
     key: 'sketch-threejs',
     title: 'Sketch of Three.js',
     description: 'Interactive sketches made with three.js.\nI start to create the sketches from 2015 and continue even now.',
-    img: thumbSketchThreejs,
+    img: '@/assets/img/webgl/thumb_sketch_threejs.jpg',
     href: 'https://ykob.github.io/sketch-threejs/',
   },
   {
     key: 'warpdrive',
     title: 'WarpDrive',
     description: 'It is the Chrome Extension, a data visualization of a user\'s active history on the browser.\nI was responsible for one of the contents named REQUEST VIEWER.\nI devised how to mix 2D and 3D user interfaces here.',
-    img: thumbWarpdrive,
+    img: '@/assets/img/webgl/thumb_warpdrive.jpg',
     href: 'https://warpdrive-project.jp/',
   },
   {
     key: 'hassyadai',
     title: 'HASSYADAI Inc.',
     description: 'It is a brand site of the agent in Japan made with various WebGL effects.\nAnd I got CSSDA WOTD for the first time by making this site.',
-    img: thumbHassyadai,
+    img: '@/assets/img/webgl/thumb_hassyadai.jpg',
     href: 'https://hassyadai.com',
   },
 ]
