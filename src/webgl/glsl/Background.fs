dabbcb72296a35f60c32c2a2897b9d587b2c82c9
yoichi kobayashi <koba@tsumikiinc.com>
2019-12-25 09:25:19 +0900

    update the background.

--- src/webgl/glsl/Background.fs
+++ src/webgl/glsl/Background.fs
@@ -21,21 +21,20 @@ void main() {
   );
   vec2 p = imgUv * 2.0 - 1.0;
 
-  float noise1 = texture2D(noiseTex, imgUv * 1.0 - vec2(0.0, time * 0.1)).r;
-  float noise2 = texture2D(noiseTex, imgUv * 2.0 + vec2(0.0, time * 0.2)).g;
-  float noise3 = texture2D(noiseTex, imgUv * 4.0 - vec2(0.0, time * 0.3)).b;
-  float noise = (noise1 * 0.6 + noise2 * 0.3 + noise3 * 0.1);
+  float noiseA1 = texture2D(noiseTex, imgUv * 1.0 - vec2(0.0, time * 0.04)).r;
+  float noiseA2 = texture2D(noiseTex, imgUv * 2.0 - vec2(time * 0.01, time * 0.08)).r;
+  float noiseA3 = texture2D(noiseTex, imgUv * 8.0 - vec2(time * 0.16), 0.0).r;
+  float noiseB1 = texture2D(noiseTex, imgUv * 1.0 + vec2(0.0, time * 0.04)).g;
+  float noiseB2 = texture2D(noiseTex, imgUv * 2.0 + vec2(time * 0.01, time * 0.08)).g;
+  float noiseB3 = texture2D(noiseTex, imgUv * 8.0 + vec2(time * 0.16, 0.0)).g;
+  float noise = (noiseA1 * 0.7 + noiseA2 * 0.2 + noiseA3 * 0.1 + noiseB1 * 0.7 + noiseB2 * 0.2 + noiseB3 * 0.1) / 2.0;
 
-  vec3 rgb1 = convertHsvToRgb(mix(
-    vec3(0.05, 0.3, 0.25),
-    vec3(0.1, 0.3, 0.05),
-    vUv.y + (1.0 - smoothstep(0.1, 0.9, noise)) * 0.4
-    ));
+  float colorNoise = texture2D(noiseTex, imgUv + vec2(0.0, time * 0.1)).b;
 
-  vec3 rgb2 = convertHsvToRgb(mix(
-    vec3(0.05, 0.3, 0.25),
-    vec3(0.1, 0.3, 0.05),
-    vUv.y + (1.0 - smoothstep(0.1, 0.9, noise)) * 0.4
+  vec3 rgb = convertHsvToRgb(mix(
+    vec3(0.11 - colorNoise * 0.04, 0.6 - colorNoise * 0.3, 0.05),
+    vec3(0.11 - colorNoise * 0.04, 0.6 - colorNoise * 0.3, 0.4),
+    smoothstep(0.0, 0.9, length(p)) * (1.0 - smoothstep(0.1, 0.9, noise))
     ));
 
   float noise4 = texture2D(noiseTex, imgUv * 0.4 + vec2(time * 0.01, 0.0)).r;
@@ -43,8 +42,6 @@ void main() {
   float noiseB = 1.0 - smoothstep(0.3, 0.7, noise4 * 0.5 + noise5 * 0.5);
 
   float noiseAlpha = clamp(alpha * 2.0 - noiseB, 0.0, 1.0);
-
-  vec3 rgb = mix(rgb1, rgb2, noiseAlpha);
 
   gl_FragColor = vec4(rgb, 1.0);
 }
