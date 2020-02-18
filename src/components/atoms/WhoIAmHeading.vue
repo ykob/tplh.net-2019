3ca0e29e54f478c20b8b479acd0e11bc67a02a99
yoichi kobayashi <info@tplh.net>
2020-02-18 16:03:00 +0900

    added the show/hide animation of the who i am heading.

--- src/components/atoms/WhoIAmHeading.vue
+++ src/components/atoms/WhoIAmHeading.vue
@@ -6,9 +6,10 @@ export default {
 
 <template lang="pug">
   h1.p-whoiam-heading
-    |Yoichi
-    br
-    |Kobayashi
+    span.p-whoiam-heading__row
+      |Yoichi
+    span.p-whoiam-heading__row
+      |Kobayashi
 </template>
 
 <style lang="scss">
@@ -21,6 +22,31 @@ export default {
     margin-left: 50%;
   }
   @include l-mobile {
+  }
+  &__row {
+    display: block;
+
+    // Transition
+    transition-property: opacity, transform;
+    transform-origin: left bottom;
+    .view-enter & {
+      opacity: 0;
+      transform: translate3d(-20px, 50px, 0) skewY(5deg);
+    }
+    .view-enter-to & {
+      opacity: 1;
+      transition-duration: 1s;
+      transition-timing-function: $easeOutCubic;
+      @include iterateTransitionDelay(2, 0.1, 0.9);
+    }
+    .view-leave-to & {
+      opacity: 0;
+      transform: translate3d(0, -60px, 0);
+      transform-origin: top;
+      transition-duration: .6s;
+      transition-timing-function: $easeInQuad;
+      @include iterateTransitionDelay(2, 0.05, 0.04);
+    }
   }
 }
 </style>
