9dc37619ef571e011d7e2dc70a1fa7000e4b4fb4
yoichi kobayashi <info@tplh.net>
2020-03-04 02:41:14 +0900

    added BundleAnalyzerPlugin

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -52,7 +54,10 @@ module.exports = {
           }
         }
       ]
-    }
+    },
+    plugins: [
+      new BundleAnalyzerPlugin()
+    ]
   },
   chainWebpack(config) {
     config.plugins.delete('prefetch');
